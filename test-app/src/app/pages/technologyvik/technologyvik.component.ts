fb7daee102c8040909fed6a74e8db79ddfe92a99
Maxon1214 <Madoff2007@mail.ru>
2022-08-30 18:06:48 +0300

    30.08.2022.1

--- test-app/src/app/pages/technologyvik/technologyvik.component.ts
+++ test-app/src/app/pages/technologyvik/technologyvik.component.ts
@@ -5,7 +5,7 @@ import {Component, OnInit} from '@angular/core';
   templateUrl: './technologyvik.component.html',
   styleUrls: ['./technologyvik.component.scss']
 })
-export class TechnologyvikComponent {
+export class TechnologyvikComponent implements OnInit{
 
   title = 'test-app';
 
@@ -18,6 +18,11 @@ export class TechnologyvikComponent {
   fio ='';
   klemo ='';
   
+  ngOnInit(){
+    this.test2();
+    this.test3();
+  }
+  
   test() {
     console.log(77);
     localStorage.setItem('Normativnidokyment1', this.Normativnidokyment1); 
@@ -28,6 +33,27 @@ export class TechnologyvikComponent {
     localStorage.setItem('nomeprotokola', this.nomeprotokola);
     localStorage.setItem('fio', this.fio);
     localStorage.setItem('klemo', this.klemo);
-    
   }
+
+  test2() {
+    const a= [17,22,9,15,1]
+    let cumma =0
+    let countElementsArr = a.length;
+    for (let i=0; i < a.length; i++){
+    cumma= cumma+a[i];
+    }
+    console.log(cumma/countElementsArr);
+
   }
+
+  test3(){
+
+    let num;
+
+    num = 5;
+
+    console.log(num);
+
+
+  }
+}
